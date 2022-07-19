de23a3199e1fbd2bcba29fc8b7afc26a74956593
benjacoblee <benjacoblee@gmail.com>
2022-07-19 11:27:47 +0800

    chore: use composition api

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,6 @@
   <NewsHome />
 </template>
 
-<script>
+<script setup>
 import NewsHome from "./components/NewsHome.vue";
-
-export default {
-  name: "App",
-  components: {
-    NewsHome,
-  },
-};
 </script>
